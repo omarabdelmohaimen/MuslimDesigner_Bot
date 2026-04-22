05885cc2d4ced63f04b6ef5242d3a498f1781caa
Omar Abdelmohaimen <oma877344@gmail.com>
2026-04-22 22:25:48 +0200

    Delete utils directory

--- utils/states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-class AdminStates(StatesGroup):
-    choosing_category = State()
-    choosing_subcategory = State()
-    choosing_identifier = State()
-    uploading_media = State()
-    entering_caption = State()
-    broadcasting = State()
